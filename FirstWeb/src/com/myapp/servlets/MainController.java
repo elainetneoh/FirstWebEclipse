130be6dd7e1e524ff587db6a2e89a4089d4fe754
Bee Ee <Bee Ee@LAPTOP-3FTK6SKC>
2020-06-04 18:31:48 +0800

    first commit

---
+++ FirstWeb/src/com/myapp/servlets/MainController.java
@@ -0,0 +1,43 @@
+package com.myapp.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(description = "This is the main controller", urlPatterns = { "/hello" })
+public class MainController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MainController() {
+    	super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        String uname= "";
+		
+		if (request.getParameter("uname")!=null) {
+			uname= request.getParameter("uname");}
+		
+
+		String message="Hello "+uname+" You have succesffully loging in ";
+		request.setAttribute("message", message);
+        request.getRequestDispatcher("/Welcome.jsp").forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
