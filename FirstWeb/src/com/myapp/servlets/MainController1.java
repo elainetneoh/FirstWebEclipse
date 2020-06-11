e22e79781cc168fba2b9d97ee99bb4da737a7aa5
Bee Ee <Bee Ee@LAPTOP-3FTK6SKC>
2020-06-11 18:24:45 +0800

    new changes

--- FirstWeb/src/com/myapp/servlets/MainController1.java
+++ FirstWeb/src/com/myapp/servlets/MainController1.java
@@ -30,7 +30,7 @@ public class MainController1 extends HttpServlet {
 			uname= request.getParameter("uname");}
 		
 
-		String message="Hello Customer "+uname+" You have succesffully loging in ";
+		String message="Hello Customer "+uname+" You have succesffully logging in ";
 		request.setAttribute("message", message);
         request.getRequestDispatcher("/Welcome.jsp").forward(request, response);
 	}
